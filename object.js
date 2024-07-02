3750becb3d51d3aa37f489f194f3dabffb2d98b9
believer-15 <ujjwalsinha15@gmail.com>
2024-07-02 12:03:56 +0530

    Objects

---
+++ object.js
@@ -0,0 +1,20 @@
+const person = {}; // empty object
+
+person.firstName = "Ujjwal";
+person.lastName = "Sinha";
+person.age = 24;
+person.eyeColor = "Blue";
+
+console.log(person.firstName, person.lastName);
+
+
+const Man = new Object(); // empty object using "new object();"
+
+Man.firstName = "Saurabh";
+Man.lastName = "Amay";
+Man.age = 21;
+Man.eyeColor = "Blue";
+
+
+
+
