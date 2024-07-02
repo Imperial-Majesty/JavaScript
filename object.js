f178a36e1f3018d7f1f71355b1315ce7406cdc8b
believer-15 <ujjwalsinha15@gmail.com>
2024-07-02 14:50:53 +0530

    my first commit

--- object.js
+++ object.js
@@ -15,6 +15,23 @@ Man.lastName = "Amay";
 Man.age = 21;
 Man.eyeColor = "Blue";
 
+console.log(Man.firstName);
 
+// object constructor function for women object
+
+function Women(firstName,LastName){
+    this.first = firstName;
+    this.last = LastName;
+}
+
+const myWomen = new Women("Lucy", "Doe"); // creating myWomen object
+
+Women.prototype.nationality = "Indian"; // using prototype we can add new properties to object constructors
+
+Women.prototype.name = function(){   // using prototype we can add new method to object constructors
+    return this.first + " " + this.last;
+}
+
+console.log(myWomen.name());
 
 
