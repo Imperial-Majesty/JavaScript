b666176d058d9332fdfbfeede7fd281a415ca2ea
Imperial-Majesty <ujjwal.sinha@globallogic.com>
2024-07-04 19:41:23 +0000

    object value

--- object.js
+++ object.js
@@ -53,5 +53,40 @@ let text = Object.entries(Man);
 
 console.log(text);
 
+// convert object into map using object.entries
+
+let fruit = {Apple: 20, Pineapple: 2, Banana: 6};
+
+const myMap = new Map(Object.entries(fruit));
+
+console.log(myMap);
+
+// create an object from list of key-value pairs
+
+let Car = [
+    ["VOLVO", 1],
+    ["Mercedes", 2],
+    ["MiniCooper", 3]
+]
+
+const myObj = Object.fromEntries(Car);
+
+console.log(myObj);
+
+// values and keys return in single dimension of array
+
+let Vehicle = {
+    name: "Volkswagen",
+    model: "S3",
+    year: 2020,
+    color: "blue"
+}
+
+const keyVeh = Object.keys(Vehicle);
+
+const valVeh = Object.values(Vehicle);
+
+console.log(keyVeh);
+console.log(valVeh);
 
 
